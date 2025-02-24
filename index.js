37ac19da7f6898f042032c0dbf421a9467d9c76e
Abhay Jain <abhayforwork02@gmail.com>
2025-02-24 19:44:35 +0000

    completed

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var cors = require('cors');
+const multer = require("multer");
+const upload = multer({dest: "uploads/"})
 require('dotenv').config()
 
 var app = express();
@@ -11,6 +13,13 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post("/api/fileanalyse", upload.single("upfile"), function(req, res) {
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  })
+})
 
 
 
